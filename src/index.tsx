8983d3e7cf5f8fe4acc046d3ba597ca265ec45f1
Florian Proksch <florian.proksch@protonmail.com>
2024-04-25 12:41:20 +0200

    feat: handling multiple dialogs
    refactor code to handle multiple open modal dialogs.
    Revamped context handling to offer dialogs a separate context
    where they will always close themselves if not explicitly mentioning
    another dialog.
    
    Additionally, opening a dialog now returns a handle which can be
    used to explicitly close that dialog.

--- src/index.tsx
+++ src/index.tsx
@@ -1,43 +1,73 @@
+import {createContext, useContext, useState, ReactNode, PropsWithChildren, CSSProperties, Fragment} from 'react'
 import { createPortal } from 'react-dom'
-import {createContext, useContext, useState, ReactNode, PropsWithChildren} from 'react'
 import './ModalContainer.css'
 
+type Dialog = {
+  reference: string,
+  component: ReactNode
+}
+
+type DialogHandle = {
+  readonly reference: string,
+  close(): unknown
+}
+
 export type ModalContext = {
-  open: (component: ReactNode) => unknown,
-  close: () => unknown
+  open: (component: ReactNode) => DialogHandle,
+  close: (reference?: string) => unknown,
+  closeAll: () => unknown
 }
 
 const Context = createContext<ModalContext>({
   open: () => { throw new Error('Must be used inside context') },
   close: () => { throw new Error('Must be used inside context') },
+  closeAll: () => { throw new Error('Must be used inside context') },
 })
 
 export type ProviderProps = PropsWithChildren<{
   className?: string
-  style?: Record<string, string>
+  style?: CSSProperties
 }>
 export function Provider({ className = '', style = {}, children }: ProviderProps) {
-  const [modalComponent, setModalComponent] = useState<ReactNode | null>(null)
+  const [dialogs, setDialogs] = useState<Dialog[]>([])
 
   const onSplashClicked = e => {
-    if (e.target === e.currentTarget) setModalComponent(null)
+    if (e.target === e.currentTarget) close()
   }
 
-  return <Context.Provider value={{
-    open: (component) => {
-      setModalComponent(component)
-    },
-    close: () => {
-      setModalComponent(null)
+  function open(component: ReactNode) {
+    const reference = nextRef()
+    setDialogs(dialogs => [...dialogs, { reference, component }])
+
+    return {
+      get reference() { return reference },
+      close() {
+        close(this.reference)
+      }
     }
-  }}>
+  }
+
+  function close(reference?: string) {
+    if (reference) setDialogs(dialogs => dialogs.filter(d => d.reference !== reference))
+    else setDialogs(dialogs => dialogs.slice(1))
+  }
+
+  function closeAll() {
+    setDialogs([])
+  }
+
+  return <Context.Provider value={{ open, close, closeAll }}>
     {children}
     {createPortal(
       <div className={`react-modal-imperative__container ${className}`}
-           style={{ ...style, ...(!modalComponent ? {display: 'none'}: {}) }}
+           style={{ ...style, ...(!dialogs.length ? {display: 'none'}: {}) }}
       >
         <div className="react-modal-imperative__wrapper" onClick={onSplashClicked}>
-          {modalComponent}
+          {dialogs.map(d => <Fragment key={d.reference}>
+            <Context.Provider value={{ open, closeAll, close: (reference => close(reference ?? d.reference)) }}>
+              {d.component}
+            </Context.Provider>
+          </Fragment>)}
         </div>
       </div>
       ,
@@ -47,4 +77,9 @@ export function Provider({ className = '', style = {}, children }: ProviderProps
 
 export function useModal() {
   return useContext(Context)
+}
+
+let refCount = 1
+function nextRef() {
+  return `${refCount++}`
 }
